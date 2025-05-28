aab86c4f2c50f560cc35600f3cca3228b165887d
gouravsharma-00 <gourav.sharma.glamat2932@gmail.com>
2025-05-28 09:32:06 +0000

    react and typescript

---
+++ app/App.tsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0)
+  const handleClick = async () => {
+
+    let [tab] = chrome.tabs.query({active : true});
+    chrome.scripting.executeScript({
+      target : {tabId : tab.id},
+      func : () => {
+        alert("Ubuntu extension")
+      }
+    })
+    setCount(prev => prev+1)
+  }
+  return (
+    <section>
+      <span>Hello, from Ubuntu {count}</span>
+      <button onClick={handleClick}>
+        click
+      </button>
+    </section>
+  );
+};
+
+export default App;
